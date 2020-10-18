6db47c63186c188b85dbca7ed840c4e4ad6415b4
Andy <aste.rix@icloud.com>
2020-10-18 22:34:40 +0200

    adds models

---
+++ src/models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        maxLength: 20
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    destinations: [{
+        type: mongoose.Types.ObjectId,
+        required: true,
+        ref: 'Destination'
+    }]
+},
+    {
+        timestamps: true
+    }
+);
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
