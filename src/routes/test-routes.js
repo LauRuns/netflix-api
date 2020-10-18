3d2d4e576bbb761672fee2cc06c8eeb443fc9006
Andy <aste.rix@icloud.com>
2020-10-18 22:34:51 +0200

    adds routes

---
+++ src/routes/test-routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const testController = require('../controllers/test-controller');
+
+
+router.get('/', testController.getTestResults);
+
+
+module.exports = router;
