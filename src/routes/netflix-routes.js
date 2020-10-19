e59f9f1ef17d6c9e9f34662cfce10aebfc7d724a
Andy <aste.rix@icloud.com>
2020-10-19 15:07:09 +0200

    adds netflix routes

--- src/routes/netflix-routes.js
+++ src/routes/netflix-routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { check } = require('express-validator');
 
+
 const router = express.Router();
 
 const netflixController = require('../controllers/netflix-controller');
@@ -9,6 +10,12 @@ const checkAuth = require('../middleware/check-auth');
 // router.use(checkAuth);
 
 router.get('/', netflixController.getNetflixItems);
+router.get('/countries', netflixController.getNetflixCountries);
+router.get('/search', netflixController.searchNetflixDB);
+router.get('/search/actor', netflixController.searchNetflixDBForActor);
+router.get('/search/deleteditems', netflixController.searchNetflixDBForDeletedItems);
+router.get('/search/netflixid', netflixController.getCountriesForID);
+router.get('/search/expiring', netflixController.getExpiring);
 
 // router.get('/:nid', netflixController.getNetflixItemById);
 // router.get('/user/:uid', netflixController.getNetflixItemsByUserId);
