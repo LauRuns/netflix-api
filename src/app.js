8f6a26fc701460fe07eff120b493e6d2fde1da67
Andy <aste.rix@icloud.com>
2020-10-31 13:07:17 +0100

    adds static img loading

--- src/app.js
+++ src/app.js
@@ -6,13 +6,11 @@ const path = require('path');
 
 const HttpError = require('./models/http-error');
 
-
 // Route references
 const netflixRoutes = require('./routes/netflix-routes');
 const destinationRoutes = require('./routes/destination-routes');
 const userRoutes = require('./routes/users-routes');
 const testRoutes = require('./routes/test-routes');
-
 
 const app = express();
 
@@ -20,24 +18,28 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 // Returns static files - requested images
-app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use(
+	'/src/uploads/images',
+	express.static(path.join('src', 'uploads', 'images'))
+);
 
 // Handle CORS - prior to passing it to the routes
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    if (req.method === 'OPTIONS') {
-        res.header(
-            'Access-Control-Allow-Methods',
-            'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-        );
-    }
-    next();
-});
+	console.log(req.originalUrl);
 
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	if (req.method === 'OPTIONS') {
+		res.header(
+			'Access-Control-Allow-Methods',
+			'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+		);
+	}
+	next();
+});
 
 // Routes to handle requests
 app.use('/api/netflix', netflixRoutes);
@@ -47,30 +49,25 @@ app.use('/api/test', testRoutes);
 
 // Middleware for unsupported routes
 app.use((req, res, next) => {
-    next(new HttpError('Unsupported route', 404));
+	next(new HttpError('Unsupported route', 404));
 });
-
 
 // General Error handling
 app.use((error, req, res, next) => {
+	// Remove file if a validation error occurrs during sign-up
+	if (req.file) {
+		fs.unlink(req.file.path, (err) => {
+			console.log('File deletion:', err);
+		});
+	}
 
-    // Remove file if a validation error occurrs during sign-up
-    if (req.file) {
-        fs.unlink(req.file.path, err => {
-            console.log('File deletion:', err);
-        });
-    }
+	if (res.headerSent) {
+		return next(error);
+	}
 
-    if (res.headerSent) {
-        return next(error);
-    }
-
-    res.status(500).json({
-        message: error.message || 'An unexpected error occurred'
-    });
-
+	res.status(500).json({
+		message: error.message || 'An unexpected error occurred'
+	});
 });
-
-
 
 module.exports = app;
