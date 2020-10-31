846194fb1ad8e132834f38f3593da3995c90b116
Andy <aste.rix@icloud.com>
2020-10-31 13:08:49 +0100

    adds country property to user

--- src/models/user.js
+++ src/models/user.js
@@ -2,35 +2,40 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 6,
-        maxLength: 20
-    },
-    image: {
-        type: String
-    },
-    destinations: [{
-        type: mongoose.Types.ObjectId,
-        required: true,
-        ref: 'Destination'
-    }]
-},
-    {
-        timestamps: true
-    }
+const userSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true
+		},
+		password: {
+			type: String,
+			required: true,
+			minLength: 6,
+			maxLength: 20
+		},
+		image: {
+			type: String
+		},
+		country: {
+			type: String
+		},
+		destinations: [
+			{
+				type: mongoose.Types.ObjectId,
+				required: true,
+				ref: 'Destination'
+			}
+		]
+	},
+	{
+		timestamps: true
+	}
 );
 
 userSchema.plugin(uniqueValidator);
