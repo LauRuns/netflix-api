b8002e03c5779efe97637b0be2910045908be5de
Andy <aste.rix@icloud.com>
2021-01-06 17:46:56 +0100

    adds mailing after signup

--- src/controllers/users-controllers.js
+++ src/controllers/users-controllers.js
@@ -5,6 +5,14 @@ const bcyrpt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { validationResult } = require('express-validator');
 
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+let defaultClient = SibApiV3Sdk.ApiClient.instance;
+let apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.SENDINBLUE;
+
+let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
 
@@ -131,6 +139,47 @@ const signup = async (req, res, next) => {
 		);
 	} catch (error) {
 		return next(new HttpError(`An error occurred: ${error.message}`, 500));
+	}
+
+	const idx = new Date().toISOString();
+
+	try {
+		sendSmtpEmail.subject = 'Jtaclogs signup confirmation';
+		sendSmtpEmail.to = [{ email: `${email}`, name: `${name}` }];
+		sendSmtpEmail.replyTo = {
+			email: process.env.JTACLOGS_ADMIN,
+			name: 'Jtaclogs admin'
+		};
+		sendSmtpEmail.htmlContent = `<html>
+				\
+				<body>
+					\<h3>Succesful sign up for ${name} with Jtaclogs!</h3>\
+					<div>
+						Thanks for signing up. Be aware that the app running on Jtaclogs is
+						using a third party API: unogsNg from ${pocess.env.RAPIDAPI} for fetching the Netflix content. We do not
+                        control the outcome or sudden changes made by this third party.
+                        <br />
+                        We also would like to point out that for some countries there is no Netflix data available. Which countries this concerns is unclear at the moment. Should you not be able to find the country of your choice, then there probably is no content available for it.
+						<br />
+						<br />
+						We hope you enjoy our app and its content.
+						<br />
+						<br />
+						Greetings from the Jatclogs team! (...which is actually just one
+						person)
+					</div>
+					\
+				</body>
+				\
+			</html>`;
+		sendSmtpEmail.sender = {
+			name: 'Jtaclogs admin',
+			email: process.env.JTACLOGS_ADMIN
+		};
+
+		await apiInstance.sendTransacEmail(sendSmtpEmail);
+	} catch (error) {
+		console.log('Error sending email______>', error);
 	}
 
 	return res.status(201).json({
