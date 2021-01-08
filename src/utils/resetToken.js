26a821dd8be4abb49b5cd0bf78747b33b303ad1c
Andy <aste.rix@icloud.com>
2021-01-08 11:22:14 +0100

    adds fucntion for creating reset token

---
+++ src/utils/resetToken.js
@@ -0,0 +1,17 @@
+const crypto = require('crypto');
+
+const generateResetToken = async () => {
+	const buffer = await new Promise((resolve, reject) => {
+		crypto.randomBytes(32, function (err, buffer) {
+			if (err) {
+				reject('error generating token');
+				return console.log('Crypto could not be set', err.message);
+			}
+			resolve(buffer);
+		});
+	});
+	const token = crypto.createHash('sha1').update(buffer).digest('hex');
+	return token;
+};
+
+exports.generateResetToken = generateResetToken;
