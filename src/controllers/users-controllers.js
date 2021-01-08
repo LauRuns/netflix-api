2c48e4cc2e136413a6d66f6050c513d6c4e04e49
Andy <aste.rix@icloud.com>
2021-01-08 11:20:47 +0100

    moves mailing function to separate file

--- src/controllers/users-controllers.js
+++ src/controllers/users-controllers.js
@@ -1,17 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const multer = require('multer');
 const bcyrpt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { validationResult } = require('express-validator');
 
-const SibApiV3Sdk = require('sib-api-v3-sdk');
-let defaultClient = SibApiV3Sdk.ApiClient.instance;
-let apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = process.env.SENDINBLUE;
-
-let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+const mailer = require('../utils/mailer');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -58,7 +49,6 @@ const getUserById = async (req, res, next) => {
 };
 
 const signup = async (req, res, next) => {
-	console.log('_____SIGN UP REQUEST RECEIVED_____');
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		console.log(errors);
@@ -141,53 +131,20 @@ const signup = async (req, res, next) => {
 		return next(new HttpError(`An error occurred: ${error.message}`, 500));
 	}
 
-	const idx = new Date().toISOString();
+	if (token) {
+		try {
+			await mailer.sendSignUpMail({ name: name, email: email });
+		} catch (error) {
+			console.log('Error sending email______>', error);
+		}
 
-	try {
-		sendSmtpEmail.subject = 'Jtaclogs signup confirmation';
-		sendSmtpEmail.to = [{ email: `${email}`, name: `${name}` }];
-		sendSmtpEmail.replyTo = {
-			email: process.env.JTACLOGS_ADMIN,
-			name: 'Jtaclogs admin'
-		};
-		sendSmtpEmail.htmlContent = `<html>
-				\
-				<body>
-					\<h3>Succesful sign up for ${name} with Jtaclogs!</h3>\
-					<div>
-						Thanks for signing up. Be aware that the app running on Jtaclogs is
-						using a third party API: unogsNg from ${process.env.RAPIDAPI} for fetching the Netflix content. We do not
-                        control the outcome or sudden changes made by this third party.
-                        <br />
-                        We also would like to point out that for some countries there is no Netflix data available. Which countries this concerns is unclear at the moment. Should you not be able to find the country of your choice, then there probably is no content available for it.
-						<br />
-						<br />
-						We hope you enjoy our app and its content.
-						<br />
-						<br />
-						Greetings from the Jatclogs team! (...which is actually just one
-						person)
-					</div>
-					\
-				</body>
-				\
-			</html>`;
-		sendSmtpEmail.sender = {
-			name: 'Jtaclogs admin',
-			email: process.env.JTACLOGS_ADMIN
-		};
-
-		await apiInstance.sendTransacEmail(sendSmtpEmail);
-	} catch (error) {
-		console.log('Error sending email______>', error);
+		return res.status(201).json({
+			user: newUser,
+			userId: createdUser.id,
+			email: createdUser.email,
+			token: token
+		});
 	}
-
-	return res.status(201).json({
-		user: newUser,
-		userId: createdUser.id,
-		email: createdUser.email,
-		token: token
-	});
 };
 
 const login = async (req, res, next) => {
