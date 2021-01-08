47b855d8743ba95eb5c01be9f567c5f556b5eb50
Andy <aste.rix@icloud.com>
2021-01-08 11:21:12 +0100

    adds routes

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const router = express.Router();
-
 const { check } = require('express-validator');
-
 const authController = require('../controllers/auth-controller');
 
+/* Reset route should always be avaliable */
+router.post(
+	'/reset',
+	[check('email').normalizeEmail().isEmail()],
+	authController.sendResetPwdLink
+);
+
+router.post(
+	'/reset/pwd/:token',
+	check('newPassword').isLength({ min: 5 }),
+	authController.resetPwd
+);
 // Setting the Auth middleware; route below should only be available when authorized
 // uncomment  next line to enable authentication for routes
-// router.use(checkAuth);
+router.use(checkAuth);
 
 router.patch(
 	'/update/:uid',
-	[
-		check('email').normalizeEmail().isEmail(),
-		check('oldPassword').isLength({ min: 5 })
-	],
+	[check('email').normalizeEmail().isEmail()],
+	check('newPassword').isLength({ min: 5 }),
 	authController.updatePwd
 );
 
